e348b7985ed87ff7969cf8eee4049b98d6b660af
sarthak19-qait <sarthaksaxena@qainfotech.com>
2019-11-03 12:08:25 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,87 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
+import Person from './Person/Person'
 
-function App() {
+class App extends Component {
+  state = {
+    persons :[
+      {id: 1, name: "Sarthak", age: 22},
+      {id: 2, name: "Somil", age: 23},
+      {id: 3, name: "Sarita", age: 24}
+    ],
+    Hobby: "cricket",
+    showPersons: false
+  }
+
+  deletePersonsHandler = (index, id)=>{
+    //This is not the best way as we are just copying the refence not the data
+    //const persons = this.state.persons
+    //This is the netter way it copies whole datainto persons
+    // const persons = this.state.persons.slice()
+    const persons = [...this.state.persons]
+    persons.splice(index,1)
+    this.setState({persons:persons})
+  }
+
+  nameChangeHandler = (event,id)=>{
+    const personIndex = this.state.persons.findIndex(p=>{
+      return p.id === id
+    })
+    const person = {
+      ...this.state.persons[personIndex]
+    }
+    person.name = event.target.value  
+    const persons = [...this.state.persons]
+    persons[personIndex] = person
+    this.setState({
+      persons : persons
+    })
+  }
+
+  toggleHandler = ()=>{
+    const condition = this.state.showPersons
+    this.setState({showPersons: !condition})
+  }
+
+  style = {
+    backgroundColor: 'white',
+    font: 'inherit',
+    border: '1px solid blue',
+    padding: '8px',
+    cursor: 'pointer'
+  }
+
+  render(){
+    let persons = null
+    if(this.state.showPersons){
+    persons = (
+      <div>
+        {this.state.persons.map((person,index)=>{
+          return <Person 
+            key = {person.id}
+            clickMe = {()=>this.deletePersonsHandler(index)}  
+            name = {person.name} 
+            age = {person.age}
+            changed = {(event)=>this.nameChangeHandler(event, person.id)}/>          
+        })}
+      </div>
+  )
+    }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Angular-Demo-4</h1>
+      <h1>I am a React App</h1>
+      <p>This is really working</p>
+      <button style = {this.style} onClick={this.toggleHandler}>Toggle</button>
+      {persons}
     </div>
+
+    // React.createElement('div', {className: 'App'}, React.createElement('h1', null,'I am a React App'))
   );
+}
 }
 
 export default App;
+
+
